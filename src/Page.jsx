1e1f81e3d97583574c7f6cc0ef17d745bbd2c4ab
esteban.sotillo <westixy@gmail.com>
2018-02-09 10:16:56 +0100

    start of project

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
+import { Button, Container, Header } from 'semantic-ui-react'
+import ChordDiagram from 'react-chord-diagram'
 
-import { Button } from 'semantic-ui-react'
+import { data } from './utils/model'
 
 export default class Page extends Component {
+  constructor(props) {
+    super(props)
+    console.log(data)
+    this.state = {
+      matrix: [
+        [10000, 5871, 8916, 2868],
+        [10000, 10048, 2060, 6171],
+        [10000, 16145, 8090, 8045],
+        [10000, 990, 940, 6907],
+      ],
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      matrix: this.state.matrix.map(row =>
+        row.map(elem => Math.random() * 10000)
+      ),
+    })
+  }
+
   render() {
     return (
-      <div className="Page">
-        <Button content="Hey Semantic !" circular />
-      </div>
+      <Container className="Page">
+        <Header as="h1">
+          Where people goes <small>PRW3 ESO</small>
+        </Header>
+        <Button
+          content="Hey Semantic !"
+          circular
+          onClick={() => this.handleClick()}
+        />
+        <ChordDiagram
+          matrix={this.state.matrix}
+          componentId={1}
+          groupLabels={['Black', 'Yellow', 'Brown', 'Orange']}
+          groupColors={['#000000', '#FFDD89', '#957244', '#F26223']}
+        />
+      </Container>
     )
   }
 }
--- src/utils/model.js
+++ src/utils/model.js
@@ -1,3 +1,5 @@
+import * as Data from '../data/trajets-des-voyageurs-entre-les-cantons-suisses1.json'
+
 export const operator = {
   reduce: { count: (a, b) => a.anzahl_reisende + b.anzahl_reisende },
   filter: {
@@ -7,14 +9,14 @@ export const operator = {
       ziel_kanton: cantonEnd,
       reisedatum: date,
     }),
-
   },
 }
 
 export class Model {
   constructor(data) {
+    console.log(data)
     this.data = data
-    this.records = this.data.records.map(a => a.fields)
+    this.records = this.data.map(a => a.fields)
   }
 
   filter(filter) {
@@ -23,11 +25,32 @@ export class Model {
         if (filter[key] === undefined) continue
         if (filter[key] instanceof RegExp) {
           if (!filter[key].test()) return false
-        } else if (filter[key] != a[key]) return false
+        } else if (filter[key] !== a[key]) return false
       }
       return true
     })
   }
 }
 
-export const data = new Model(import('../data/trajets-des-voyageurs-entre-les-cantons-suisses1.json')) 
+export class Travels {
+  constructor() {
+    this.model = new Model(Data)
+    this.filters = {}
+  }
+
+  get cantonList(){
+    let cantons = {}
+    this.model.records.forEach(a=>cantons[a.start_kanton]=1)
+    return Object.keys(cantons)
+  }
+
+  get dateList(){
+      let dates = {}
+      this.model.records.forEach(a=>dates[a.reisedatum]=1)
+      return Object.keys(dates).sort()
+  }
+
+
+}
+
+export const data = new Travels()
